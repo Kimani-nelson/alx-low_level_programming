3e298c9cd15b5222933e4986b79c0f0af72edcf5
teargas001 <nkimnjoro@gmail.com>
2023-07-10 12:12:42 -0700

    Elf

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -76,7 +76,7 @@ void print_magic(unsigned char *e_ident)
 void print_class(unsigned char *e_ident)
 
 {
-	printf(" Class: ");
+	printf("  Class: ");
 
 	switch (e_ident[EI_CLASS])
 	{
@@ -102,7 +102,7 @@ void print_class(unsigned char *e_ident)
 void print_data(unsigned char *e_ident)
 
 {
-	printf(" Data: ");
+	printf("Data:");
 
 	switch (e_ident[EI_DATA])
 	{
@@ -124,10 +124,12 @@ void print_data(unsigned char *e_ident)
  * print_version - Prints the version of an ELF header.
  * @e_ident: A pointer to an array containing the ELF version.
  */
+
 void print_version(unsigned char *e_ident)
+
 {
-	 printf(" Version: %d",
-			  e_ident[EI_VERSION]);
+	printf("Version:%d",
+	       e_ident[EI_VERSION]);
 
 	switch (e_ident[EI_VERSION])
 	{
@@ -148,7 +150,7 @@ void print_version(unsigned char *e_ident)
 void print_osabi(unsigned char *e_ident)
 
 {
-	printf(" OS/ABI: ");
+	printf("OS/ABI:");
 
 	switch (e_ident[EI_OSABI])
 	{
@@ -195,8 +197,8 @@ void print_osabi(unsigned char *e_ident)
 void print_abi(unsigned char *e_ident)
 
 {
-	printf(" ABI Version: %d\n",
-		e_ident[EI_ABIVERSION]);
+	printf("ABI Version:%d\n",
+	       e_ident[EI_ABIVERSION]);
 }
 
 /**
@@ -211,7 +213,7 @@ void print_type(unsigned int e_type, unsigned char *e_ident)
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
 		e_type >>= 8;
 
-	printf(" Type: ");
+	printf("Type:");
 
 	switch (e_type)
 	{
@@ -244,7 +246,7 @@ void print_type(unsigned int e_type, unsigned char *e_ident)
 void print_entry(unsigned long int e_entry, unsigned char *e_ident)
 
 {
-	printf(" Entry point address: ");
+	printf("Entry point address:");
 
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
 	{
@@ -280,14 +282,14 @@ void close_elf(int elf)
 
 /**
  * main - Displays the information contained in the
- * ELF header at the start of an ELF file.
+ *        ELF header at the start of an ELF file.
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
  *
  * Return: 0 on success.
  *
  * Description: If the file is not an ELF File or
- * the function fails - exit code 98.
+ *              the function fails - exit code 98.
  */
 
 int main(int __attribute__((__unused__)) argc, char *argv[])
